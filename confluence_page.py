357658ba5e55f30aa0f51be80aff9f6da65372ef
RomainNeup <71385790+RomainNeup@users.noreply.github.com>
2025-03-01 20:59:02 +0100

    Personal Access Token authentication and user settings

--- confluence_page.py
+++ confluence_page.py
@@ -6,11 +6,12 @@ author: @romainneup
 author_url: https://github.com/RomainNeup
 funding_url: https://github.com/sponsors/RomainNeup
 requirements: markdownify
-version: 0.1.0
+version: 0.1.1
 changelog:
 - 0.0.1 - Initial code base.
 - 0.0.2 - Fix Valves variables
 - 0.1.0 - Split Confuence search and Confluence get page
+- 0.1.1 - Add support for Personal Access Token authentication and user settings
 """
 
 import base64
@@ -55,14 +56,18 @@ class EventEmitter:
 
 
 class Confluence:
-    def __init__(self, username: str, api_key: str, base_url: str):
+    def __init__(
+        self, username: str, api_key: str, base_url: str, api_key_auth: bool = True
+    ):
         self.base_url = base_url
-        self.headers = self.authenticate(username, api_key)
+        self.headers = self.authenticate(username, api_key, api_key_auth)
         pass
 
     def get(self, endpoint: str, params: Dict[str, Any]) -> Dict[str, Any]:
         url = f"{self.base_url}/rest/api/{endpoint}"
         response = requests.get(url, params=params, headers=self.headers)
+        if not response.ok:
+            raise Exception(f"Failed to get data from Confluence: {response.text}")
         return response.json()
 
     def get_page(self, page_id: str) -> Dict[str, Any]:
@@ -76,12 +81,21 @@ class Confluence:
             "link": f"{self.base_url}{result['_links']['webui']}",
         }
 
-    def authenticate(self, username: str, api_key: str) -> Dict[str, str]:
+    def authenticate_api_key(self, username: str, api_key: str) -> Dict[str, str]:
         auth_string = f"{username}:{api_key}"
         encoded_auth_string = base64.b64encode(auth_string.encode("utf-8")).decode(
             "utf-8"
         )
         return {"Authorization": "Basic " + encoded_auth_string}
+
+    def authenticate_personal_access_token(self, access_token: str) -> Dict[str, str]:
+        return {"Authorization": f"Bearer {access_token}"}
+
+    def authenticate(self, username: str, api_key: str, api_key_auth: bool) -> Dict[str, str]:
+        if api_key_auth:
+            return self.authenticate_api_key(username, api_key)
+        else:
+            return self.authenticate_personal_access_token(api_key)
 
 
 class Tools:
@@ -90,12 +104,33 @@ class Tools:
         pass
 
     class Valves(BaseModel):
-        username: str = Field("example@example.com", description="Your username here")
-        api_key: str = Field("ABCD1234", description="Your API key here")
         base_url: str = Field(
             "https://example.atlassian.net/wiki",
             description="The base URL of your Confluence instance",
         )
+        username: str = Field(
+            "example@example.com",
+            description="Default username (leave empty for personal access token)",
+        )
+        api_key: str = Field(
+            "ABCD1234", description="Default API key or personal access token"
+        )
+        pass
+
+    class UserValves(BaseModel):
+        api_key_auth: bool = Field(
+            True,
+            description="Use API key authentication; disable this to use a personal access token instead.",
+        )
+        username: str = Field(
+            "",
+            description="Username, typically your email address; leave empty if using a personal access token or default settings.",
+        )
+        api_key: str = Field(
+            "",
+            description="API key or personal access token; leave empty to use the default settings.",
+        )
+        pass
 
     # Get content from Confluence
     async def get_confluence_page(
@@ -110,8 +145,29 @@ class Tools:
         :param page_id: The ID of the page on Confluence
         :return: The content of the page on Confluence in JSON format (title, body, link). If the page is not found, an error message is returned.
         """
-        confluence = Confluence(self.valves.username, self.valves.api_key, self.valves.base_url)
         event_emitter = EventEmitter(__event_emitter__)
+
+        # Get the username and API key
+        if __user__ and "valves" in __user__:
+            user_valves = __user__["valves"]
+            api_key_auth = user_valves.api_key_auth
+            api_username = user_valves.username or self.valves.username
+            api_key = user_valves.api_key or self.valves.api_key
+        else:
+            api_username = self.valves.username
+            api_key = self.valves.api_key
+            api_key_auth = True
+        
+        if (api_key_auth and not api_username) or not api_key:
+            await event_emitter.emit_status(
+                "Please provide a username and API key or personal access token.", True, True
+            )
+            return "Error: Please provide a username and API key or personal access token."
+        
+        confluence = Confluence(
+            api_username, api_key, self.valves.base_url, api_key_auth
+        )
+        
         await event_emitter.emit_status(f"Retrieving page '{page_id}' from Confluence...", False)
         try:
             result = confluence.get_page(page_id)
